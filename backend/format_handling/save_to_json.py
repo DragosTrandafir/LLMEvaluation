835b48cf98f6e6f3d8e612c4fd1a537c9298aa2c
DragosTrandafiri <dragostrandafir333@gmail.com>
2025-10-31 14:27:30 +0200

    added more documentation

--- backend/format_handling/save_to_json.py
+++ backend/format_handling/save_to_json.py
@@ -1,10 +1,12 @@
 import json
 
 
+# function that saves python object data into a json path file
 def save_json_format(data, path):
     try:
-        with open(path, 'w', encoding='utf-8') as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
+        with open(path, 'w', encoding='utf-8') as f:  # open json file with write access
+            json.dump(data, f, ensure_ascii=False,
+                      indent=4)  # keep readable special characters and make the data more human-readable
         return True
     except Exception as e:
         print(f"Error saving JSON to {path}: {e}")
